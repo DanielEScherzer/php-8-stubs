2b892d0c9cd7c64ddf6de28c9a002a911f1109ae
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2025-08-20 11:56:49 -0700

    Extractor: Skip `Since` and `Until` attributes
    Otherwise every version would be a change

--- extractor/extract.php
+++ extractor/extract.php
@@ -578,6 +578,9 @@ $command = new class(
 						if ($oldArg->name->name !== $newArg->name->name) {
 							return $this->stmtDiff($old, $new, $updateTo);
 						}
+						if ($oldArg->name->name === 'Since' || $oldArg->name->name === 'Until') {
+							continue;
+						}
 					} elseif ($oldArg->name !== null || $newArg->name !== null) {
 						return $this->stmtDiff($old, $new, $updateTo);
 					}
@@ -588,7 +591,7 @@ $command = new class(
 					}
 				}
 			}
-		}
+		}'
 
 		foreach ($old->getParams() as $i => $oldParam) {
 			$newParam = $new->getParams()[$i];
